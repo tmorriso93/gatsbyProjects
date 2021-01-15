8ebbdb7331efb780bba239fbdcbc1bda898e7523
tmorriso93 <tmorriso93@gmail.com>
2021-01-15 15:42:33 -0500

    added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,5 +25,30 @@ module.exports = {
         },
         `gatsby-plugin-sharp`,
          `gatsby-transformer-sharp`,
+         {
+           resolve: `gatsby-plugin-mdx`,
+           options: {
+             extensions: [`.md`, `.mdx`],
+             gatsbyRemarkPlugins: [
+               {
+                 resolve: `gatsby-remark-images`,
+                 options: {
+                   maxWidth: 1200,
+
+                 },
+               },
+             ],
+           },
+         },
+         {
+           resolve: `gatsby-plugin-google-fonts`,
+           options: {
+             fonts: [
+               `roboto mono`,
+               `muli\:400,400i,700,700i`,
+             ],
+             display: "swap",
+           },
+         },
   ],
 }
